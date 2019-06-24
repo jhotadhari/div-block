5ad39ce05a4c1a36894ba071568462e2acab1a9c
jhotadhari <tellme@waterproof-webdesign.info>
2019-06-24 08:00:57 +0545

    Build successful

---
+++ div-block.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	Plugin Name: Div Block
+	Plugin URI: https://github.com/jhotadhari/div-block
+	Description: Just a simple div Block. Nothing else.
+	Version: 0.0.0
+	Author: jhotadhari
+	Author URI: https://github.com/jhotadhari
+	License: GNU General Public License v2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+	Text Domain: divb
+	Domain Path: /languages
+	Tags: container,wrapper,block,div,gutenberg,bootstrap
+	GitHub Plugin URI: https://github.com/jhotadhari/div-block
+	Release Asset: true
+*/
+?><?php
+
+/**
+ * This is a dummy plugin file, required by GitHub Updater Plugin (https://github.com/afragen/github-updater).
+ * GitHub Updater Plugin will query this file for updates.
+ */
+
+?>
